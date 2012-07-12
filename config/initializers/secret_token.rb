5c3edee9009e5a4dc85d084c6f03c2a4b3033c65
Gregory <gregorylaflash@gmail.com>
2012-07-11 20:21:51 -0500

    Restructured files and modified GemFile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartHome::Application.config.secret_token = 'c02384c7934f9eb40b8b9308772e0ed28ca645ca4a56a4cf30621115011c9a97e841aa29cf5591f2c16895a136d8037b4ec72f6e6631b6282f91dc75701d2edb'
